75820705f84ca5e0457ae311b1e481aca1cabf33
Donatas <donkuprys@gmail.com>
2021-05-19 20:57:52 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const Dog = require('./js/Dog');
+
+const rexas = new Dog('Rex', 'brouwn');
+console.log(rexas.legsCount);
+
+
+const spaikas = new Dog('Spike', 'white');
+console.log(spaikas.name);
